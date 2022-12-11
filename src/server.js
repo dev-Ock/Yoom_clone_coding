6e1e5199bee75e7aecd5890f9d9cde79b9afae5a
_devOck <respecttowardyou@gmail.com>
2022-12-11 21:36:21 +0900

    Create Chat Code

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -16,4 +18,35 @@ app.get("/*", (req, res, next) => {
 const handleListen = () => {
   console.log("Listening on http://localhost:3000");
 };
-app.listen(3000, handleListen);
+
+// app.listen(3000, handleListen);
+
+/* 
+app.listen = function() {
+  var server = http.createServer(this);
+  return server.listen.apply(server, arguments);
+};
+*/
+
+// http 서버를 server 라는 변수에 담기 위해 app.listen 메소드 내부 코드 중 일부를 굳이 표현
+const server = http.createServer(app);
+
+// web socket 서버를 http 서버와 동일한 포트에서 열기 위해 http 서버를 변수 server에 담았고, 그 server에 web socket도 같이 띄움.
+const wss = new WebSocket.Server({ server });
+
+const allMember = [];
+
+wss.on("connection", (socket) => {
+  allMember.push(socket);
+  console.log("Connected to the Browser");
+  socket.on("close", () => {
+    console.log("Disconnected to the Brwoser");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString());
+    const stringMessage = message.toString();
+    allMember.forEach((all) => all.send(stringMessage));
+  });
+});
+
+server.listen(3000, handleListen);
