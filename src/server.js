7296ffd6e29086f0e7b40b309e4335e80e6189d6
_devOck <respecttowardyou@gmail.com>
2022-12-11 19:07:01 +0900

    Update settings

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello~!!");
+// console.log("Hello~!!");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/", express.static(__dirname + "/public"));
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+app.get("/*", (req, res, next) => {
+  res.redirect("/");
+});
 
-app.listen(3000);
+const handleListen = () => {
+  console.log("Listening on http://localhost:3000");
+};
+app.listen(3000, handleListen);
